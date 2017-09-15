4b823ba8fd8feecc2d16f470ccf20f359d193b34
yanagiis <yanagiis@gmail.com>
2017-09-16 00:27:30 +0800

    use json as payload format

--- src/services/nats_bus.py
+++ src/services/nats_bus.py
@@ -4,6 +4,7 @@
 from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 from logzero import logger
+import json
 
 
 class NatsBus(object):
@@ -15,18 +16,20 @@ class NatsBus(object):
         retry_times = 0
         while True:
             try:
-                logger.info("Try to connect to nats server '%s'", self._url)
+                logger.info("Try to connect to nats server '%s', %d times",
+                            self._url, retry_times)
                 await self._nats_client.connect(servers=[self._url])
+                break
             except ErrNoServers:
                 retry_times += 1
-                logger.error("Cannot connect to nats server '%s', retry %d",
-                             self._url, retry_times)
+                logger.error("Cannot connect to nats server '%s'", self._url)
         logger.info("Connect to nats server '%s' successfully", self._url)
 
     async def req(self, path, payload):
         if not self._nats_client.is_connected:
             return None
-        return await self._nats_client.request(path, payload)
+        return await self._nats_client.request(
+            path, json.dumps(payload).encode('utf-8'))
 
     async def reg_rep(self, path, callback):
         return await self.reg_sub(path, callback)
@@ -34,11 +37,12 @@ class NatsBus(object):
     async def pub(self, path, payload):
         if not self._nats_client.is_connected:
             return False
-        await self._nats_client.publish(path, payload)
+        await self._nats_client.publish(path,
+                                        json.dumps(payload).encode('utf-8'))
         return True
 
     async def reg_sub(self, path, callback):
         if not self._nats_client.is_connected:
             return False
-        await self._nats_client.subscribe(path, callback)
+        await self._nats_client.subscribe(path, cb=callback)
         return True
