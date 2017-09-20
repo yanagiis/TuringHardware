b4d6bfce6c7995579d52dd78922957090828fbc4
yanagiis <yanagiis@gmail.com>
2017-09-20 22:19:45 +0800

    fix test tank water test wrong service

--- src/test/test_tank_water_service.py
+++ src/test/test_tank_water_service.py
@@ -5,19 +5,19 @@ import pytest
 import asyncio
 from test.mock.pwm import MockPWM
 from test.mock.bus import MockBus
-from test.mock.temperature_sensor import MockTemperatureSensor
-from services.tank_temp_service import TankTempService
+from test.mock.water_detector import MockWaterDetector
+from services.tank_water_service import TankWaterService
 
 
 @pytest.mark.asyncio
-async def test_tank_temp_service_pub():
+async def test_tank_water_service_pub():
     async def _req_cb(path, timeout):
         assert False
 
     async def _pub_cb(path, data):
-        assert path == 'tank.temperature'
+        assert path == 'tank.water'
         assert data['status'] == 'ok'
-        assert data['temperature'] == 100
+        assert data['water'] is True
 
     async def _reg_rep_cb(path, callback):
         assert False
@@ -31,8 +31,42 @@ async def test_tank_temp_service_pub():
     bus.reg_rep_cb = _reg_rep_cb
     bus.reg_sub_cb = _reg_sub_cb
 
-    sensor = MockTemperatureSensor()
-    sensor.temp = 100
-    service = TankTempService(sensor, 1000, bus)
-    await service.pub_tank_temperature()
+    sensor = MockWaterDetector()
+    sensor._is_water_full = True
+
+    service = TankWaterService(sensor, 1000, bus)
+    await service.pub_water_status()
     assert sensor.is_connected() is True
+
+
+@pytest.mark.asyncio
+async def test_tank_water_service_command():
+    async def _req_cb(path, timeout):
+        assert False
+
+    async def _pub_cb(path, data):
+        assert path == 'tank.water'
+        assert data['status'] == 'ok'
+        assert data['water'] is True
+
+    async def _reg_rep_cb(path, callback):
+        assert False
+
+    async def _reg_sub_cb(path, callback):
+        assert False
+
+    bus = MockBus()
+    bus.req_cb = _req_cb
+    bus.pub_cb = _pub_cb
+    bus.reg_rep_cb = _reg_rep_cb
+    bus.reg_sub_cb = _reg_sub_cb
+
+    sensor = MockWaterDetector()
+    sensor._is_water_full = True
+
+    service = TankWaterService(sensor, 1000, bus)
+    await service.pub_water_status()
+    response = await service.rep_water_command({'command': 'get'})
+    assert sensor.is_connected() is True
+    assert response['status'] == 'ok'
+    assert response['water'] is True
