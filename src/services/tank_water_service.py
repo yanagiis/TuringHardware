a46101e5673edf2ac8278646d974e0649df8c734
yanagiis <yanagiis@gmail.com>
2017-09-21 16:58:03 +0800

    code refactoring

--- src/services/tank_water_service.py
+++ src/services/tank_water_service.py
@@ -12,9 +12,12 @@ class TankWaterService(object):
             scan_interval_ms (int): scan interval in milisecond
         """
         self._sensor = sensor
-        self._is_water_full = None
         self._interval = scan_interval_ms
         self._bus = bus
+
+        self._available = False
+        self._message = 'Doesn\'t start'
+        self._is_water_full = None
 
     async def start(self):
         self._bus.reg_rep('tank.water', self.rep_water_command)
@@ -24,25 +27,24 @@ class TankWaterService(object):
 
     async def pub_water_status(self):
         if not self._sensor.is_connected() and not self._sensor.connect():
-            await self._bus.pub('tank.water', {
-                "status": "error",
-                "message": "Cannot connect to sensor"
-            })
+            self._available = False
+            self._message = 'Cannot connect to water detector'
+            await self._bus.pub('tank.water', self._get_status())
             return
 
         is_water_full = self._sensor.is_water_full()
+        self._available = True
         if is_water_full != self._is_water_full:
             self._is_water_full = is_water_full
             await self._bus.pub('tank.water', self._get_status())
 
     async def rep_water_command(self, data):
-        if not self._sensor.is_connected() and not self._sensor.connect():
-            return {"status": "error", "message": "Cannot connect to sensor"}
-
         cmd = data['command']
         if cmd == 'get':
             return self._get_status()
         return {"status": "error", "message": "Unknown command '%s'" % cmd}
 
     def _get_status(self):
-        return {"status": "ok", "water": self._is_water_full}
+        if self._available is True:
+            return {"status": "ok", "water": self._is_water_full}
+        return {'status': "error", 'message': self._message}
