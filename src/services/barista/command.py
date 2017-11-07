f09b73789c21b0b7d0ccacd0f1ae96e8195b5f9a
yanagiis <yanagiis@gmail.com>
2017-11-07 14:18:54 +0800

    change toJSON to toDict

--- src/services/barista/command.py
+++ src/services/barista/command.py
@@ -7,23 +7,23 @@ class Wait(object):
     def __init__(self, time):
         self.time = time
 
-    def toJSON(self):
-        return json.dumps({"name": "wait", "time": self.time})
+    def toDict(self):
+        return {"name": "wait", "time": self.time}
 
 
 class Mix(object):
     def __init__(self, temperature):
         self.temperature = temperature
 
-    def toJSON(self):
-        return json.dumps({"name": "mix", "t": self.temperature})
+    def toDict(self):
+        return {"name": "mix", "t": self.temperature}
 
 
 class Calibration(object):
-    def toJSON(self):
-        return json.dumps({"name": "calibration"})
+    def toDict(self):
+        return {"name": "calibration"}
 
 
 class Home(object):
-    def toJSON(self):
-        return json.dumps({"name": "home"})
+    def toDict(self):
+        return {"name": "home"}
