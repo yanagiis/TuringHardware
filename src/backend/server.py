93da8ca8f9c0be7d2eb908af156f5550164caa4d
yanagiis <yanagiis@gmail.com>
2018-01-07 15:22:45 +0800

    add aiohttp IndexMiddleware

--- src/backend/server.py
+++ src/backend/server.py
@@ -2,6 +2,7 @@
 
 import asyncio
 from aiohttp import web
+from aiohttp_index import IndexMiddleware
 from backend import barista
 from backend import cookbook
 from backend import machine
@@ -28,7 +29,7 @@ async def start_backend(config, bus):
     cookbook_mgr = CookbookManager(url)
     await cookbook_mgr.connect()
 
-    app = web.Application()
+    app = web.Application(middlewares=[IndexMiddleware()])
     app['cookbook_mgr'] = cookbook_mgr
 
     app['output_temp_client'] = OutputTempClient(bus)
@@ -45,6 +46,9 @@ async def start_backend(config, bus):
     app.router.add_route('*', '/api/machine', machine.MachineView)
     app.router.add_route('*', '/api/machine/tank/temperature',
                          machine.TankTemperatureView)
+
+    app.router.add_static(
+        '/', path=str('backend/build'), name='build', show_index=True)
 
     loop = asyncio.get_event_loop()
     await loop.create_server(app.make_handler(), '0.0.0.0', 3001)
