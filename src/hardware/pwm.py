4fc0e010408db2b7211f343cdae721e98248e96f
yanagiis <yanagiis@gmail.com>
2018-02-25 17:33:13 +0800

    add hardware pwm

--- src/hardware/pwm.py
+++ src/hardware/pwm.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from lib.tio import tio
-from hardware.gpio import GPIO
 import asyncio
 import threading
 import time
+import RPi.GPIO as rGPIO
+
+from lib.tio import tio
+from hardware.gpio import GPIO
+from hardware.hardware import HARDWARE
 
 
 class PWM(tio.IO):
@@ -47,8 +50,8 @@ class SWPWM(PWM):
         self._gpio_pin = gpio_pin
         self._dutycycle = config.dutycycle
         self._freq = config.frequency
-        self._gpio = None
         self._stop = False
+        self._gpio = None
         self._stop_event = asyncio.Event()
 
     def open(self):
@@ -99,3 +102,55 @@ class SWPWM(PWM):
     async def stop(self):
         self._stop = True
         await self._stop_event.wait()
+
+
+class HWPWM(PWM):
+    """ Hardware PWM
+    """
+
+    def __init__(self, gpio_pin, config):
+        """
+        Args:
+            gpio_pin (int): gpio pin number this PWM used
+            config (PWMConfig): configuration for pwm
+        """
+        self._gpio_pin = gpio_pin
+        self._dutycycle = config.dutycycle
+        self._freq = config.frequency
+        self._gpio = None
+
+    def open(self):
+        self._gpio = GPIO.PWM(self._gpio_pin, self._freq)
+
+    def close(self):
+        self._gpio = None
+
+    @property
+    def dutycycle(self):
+        """
+        dutycycle (float): 0 - 1 (0% - 100%)
+        """
+        return self._dutycycle
+
+    @dutycycle.setter
+    def dutycycle(self, dutycycle):
+        if not 0 <= dutycycle <= 1:
+            raise ValueError("Duty cycle should be ranged in 0 and 1")
+        self._gpio.ChangeDutyCycle(dutycycle * 100)
+
+    @property
+    def frequency(self):
+        """
+        frequency (int): hz
+        """
+        return self._freq
+
+    @frequency.setter
+    def frequency(self, freq):
+        self._gpio.ChangeFrequency(freq)
+
+    async def start(self):
+        self._gpio.start()
+
+    async def stop(self):
+        self._gpio.stop()
